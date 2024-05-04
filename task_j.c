e142441d75ff02b5ee93be9f01c78dad9e9cedbb
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 15:00:37 +0100

    Add files via upload

---
+++ task_j.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+int main(void) {
+	// Set email address here
+	char email[] = "xianye.wu@student.manchester.ac.uk";
+	// 0pen file for writing
+	FILE* fptr;
+	fptr = fopen("certificate.txt", "w");
+	if (fptr == NULL) { // display error if can't open file
+		printf("Error, Not able to open the file.");
+		return EXIT_FAILURE;
+	}
+	
+	
+	// 0utput wanted text
+	fprintf(fptr, "Hello, My email address is:\n"); 
+	fprintf(fptr, "%s\n", email);
+	fprintf(fptr, "I completed the course on ");
+
+	struct tm* local;
+	time_t t;
+	t = time(NULL);
+	local = localtime(&t);
+	char date[20];
+	strftime(date, 20, "%b %d %Y", local); 
+	fprintf(fptr, "%s", date);
+	
+	
+	
+	
+	
+	// Close and tidy up
+	fclose(fptr);
+	fptr = NULL;
+	return EXIT_SUCCESS;
+}
